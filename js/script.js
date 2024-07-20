92f76bb682807aee93c5734f4ba3bc07a90b56b0
Hashan099 <hashangunathilaka99@gmail.com>
2024-07-20 15:56:50 +0530

    initial commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+const tip = document.getElementById("tipInput");
+const total = document.getElementById("billTotalInput");
+const showtotal = document.getElementById("BillTotal");
+const showtipamount = document.getElementById("perPersonTotal");
+const payableamount = document.getElementById("payabletotal");
+
+document.addEventListener("DOMContentLoaded", () => {
+  calculatebill();
+});
+
+function calculatebill() {
+  tip.addEventListener("keyup", (event) => {
+    tipamount = event.target.value;
+
+    if(!tipamount || isNaN(tipamount)) {
+      showtipamount.innerHTML = `Rs 0.00`;
+    } else{
+      totalamount = Number(total.value);
+      if(isNaN(totalamount)) {
+        total.value = 0;
+      } else {
+        const perPersonTotal = totalamount / Number(tipamount);
+        showtotal.innerHTML = ` Rs : ${Number(total.value).toFixed(2)}`;
+        showtipamount.innerHTML = ` Rs : ${perPersonTotal.toFixed(2)}`;
+
+        const payamount = Number(total.value) + perPersonTotal
+
+        payableamount.innerHTML = ` Rs : ${payamount.toFixed(2)} `
+      }
+    }
+  });
+}
